6c62bd8ea563a3da125494de506b992a78bb97db
glennhefel <mythhunter19@gmail.com>
2025-05-06 14:56:21 +0600

    Add files via upload

---
+++ media/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # ... your existing patterns ...
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+]
+from . views import homepage, media_detail, add_reviews, top100
+# add_media 
+
+from media.views import *
+#afrom .views import add_media, homepage, media_detail
+
+app_name = 'media'
+
+urlpatterns = [
+    path("", homepage, name="homepage"),
+    path("media/<int:pk>/", media_detail, name="m_detail"),
+    #path("add_media/", add_media, name="add_media"),
+    path("add_reviews/", add_reviews, name="add_reviews"),
+    path("top100/", top100, name="top100"),
+
+]
